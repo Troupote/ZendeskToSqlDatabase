4135106915384f6ce934dd53e840b9ba6615031b
Troupote <thybault.jallu@icloud.com>
2024-02-29 12:21:34 +0100

    Documentation ajoutée

--- Main.cs
+++ Main.cs
@@ -1,6 +1,26 @@
+/*
+    Ce fichier contient le code principal pour extraire les tickets Zendesk et les stocker dans une base de données SQL.
+    Il utilise l'API ZendeskApi_v2 pour récupérer les tickets et la bibliothèque System.Data.SqlClient pour se connecter à la base de données SQL.
+
+    Le code commence par récupérer les informations de configuration à partir de fichiers texte ou en les demandant à l'utilisateur.
+    Ensuite, il crée une connexion à la base de données SQL et crée les tables nécessaires.
+    Ensuite, il récupère les tickets Zendesk à l'aide de l'API ZendeskApi_v2 et les insère dans la base de données SQL.
+    Enfin, il offre à l'utilisateur la possibilité d'afficher les tickets actuels.
+
+    Assurez-vous de configurer correctement les informations de connexion à la base de données SQL et les fichiers de configuration Zendesk avant d'exécuter ce code.
+
+    Pour plus d'informations sur l'API ZendeskApi_v2, consultez la documentation officielle : https://github.com/CloudCoreo/ZendeskApi_v2
+
+    Pour plus d'informations sur la bibliothèque System.Data.SqlClient, consultez la documentation officielle : https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient?view=net-5.0
+
+    Auteur : Votre nom
+    Date : Date de création du fichier
+*/
+
 using System.Data.SqlClient;
 using ZendeskApi_v2;
 
+// Définition des chemins des fichiers de configuration
 string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 string configFolderPath = Path.Combine(baseDirectory, "config");
 string zendeskSiteFilePath = Path.Combine(configFolderPath, "zendeskSite.txt");
@@ -64,22 +84,20 @@ else
     File.WriteAllText(userTokenFilePath, userToken);
 }
 
-
-
-
 Console.WriteLine($"Le dossier de configuration est à: {configFolderPath}");
 
-
+// Création de l'objet ZendeskApi pour se connecter à l'API Zendesk
 var api = new ZendeskApi(zendeskSite, userEmail, userToken, "true");
 
+// Chaîne de connexion à la base de données SQL
 string connectionString = """Server=LROLAP\SQLEXPRESS01;Database=Zendesk_database;User Id=Wizard;Password=Wizard;""";
 
+// Connexion à la base de données SQL
 using (SqlConnection connection = new SqlConnection(connectionString))
 {
     connection.Open();
 
-    var tickets = api.Tickets.GetAllTickets();
-
+    // Création de la table Tickets si elle n'existe pas déjà
     string sqlStatement = @"
     IF OBJECT_ID('Tickets', 'U') IS NOT NULL 
     DROP TABLE Tickets;
@@ -100,6 +118,7 @@ using (SqlConnection connection = new SqlConnection(connectionString))
         command.ExecuteNonQuery();
     }
 
+    // Création de la table Users si elle n'existe pas déjà
     string createUserTableSql = @"
     IF OBJECT_ID('Users', 'U') IS NOT NULL 
     DROP TABLE Users;
@@ -116,6 +135,7 @@ using (SqlConnection connection = new SqlConnection(connectionString))
         createUserTableCommand.ExecuteNonQuery();
     }
     
+    // Récupération des utilisateurs Zendesk et insertion dans la table Users
     var groupUserResponse = api.Users.GetAllUsers();
     foreach (var user in groupUserResponse.Users)
     {
@@ -132,6 +152,8 @@ using (SqlConnection connection = new SqlConnection(connectionString))
         }
     }
 
+    // Récupération des tickets Zendesk et insertion dans la table Tickets
+    var tickets = api.Tickets.GetAllTickets();
     foreach (var item in tickets.Tickets)
     {
         sqlStatement = "INSERT INTO Tickets (Id, Priority, Type, Subject, Status, Requester_Id, Assigned_Id, Created_at, Url) VALUES (@Id, @Priority, @Type, @Subject, @Status, @Requester, @Assigned, @Created_at, @Url)";
@@ -145,22 +167,20 @@ using (SqlConnection connection = new SqlConnection(connectionString))
             command.Parameters.AddWithValue("@Priority", item.Priority);
             command.Parameters.AddWithValue("@Requester", item.RequesterId);
             command.Parameters.AddWithValue("@Assigned", item.AssigneeId);
-            command.Parameters.AddWithValue("@Type", string.IsNullOrEmpty(item.Type) ? (object)DBNull.Value : item.Type); command.Parameters.AddWithValue("@Url", item.Url);
+            command.Parameters.AddWithValue("@Type", string.IsNullOrEmpty(item.Type) ? (object)DBNull.Value : item.Type);
+            command.Parameters.AddWithValue("@Url", item.Url);
 
             command.ExecuteNonQuery();
         }
-
     }
+
+    // Affichage des tickets actuels si l'utilisateur le souhaite
     Console.WriteLine("[1] Voulez-vous afficher les tickets actuels\n[Autre touche] Quitter");
 
     if (Console.ReadLine() == "1")
     {
-
-
         foreach (var item in tickets.Tickets)
         {
-
-
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("ID du ticket: ");
             Console.ForegroundColor = ConsoleColor.White;
@@ -182,16 +202,13 @@ using (SqlConnection connection = new SqlConnection(connectionString))
             Console.Write(item.CreatedAt + " ");
             Console.WriteLine("");
             Console.WriteLine("");
-
         }
 
         Console.WriteLine("Appuyez sur une touche pour quitter");
         Console.ReadKey();
     }
-
     else
     {
         Environment.Exit(0);
     }
-
 }
